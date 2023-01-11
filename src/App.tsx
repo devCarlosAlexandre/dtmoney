42350a0c7e052547d345b81896f7a067a3221398
devCarlosALexandre <carloalexandresousa@gmail.com>
2023-01-11 10:20:54 -0300

    feat: :sparkles: criado estilos globais e importado os assets do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
+import { GlobalStyle } from './styles/global';
 export function App() {
   return (
     <div className="App">
       <h1>Hellow Word</h1>
+      <GlobalStyle />
     </div>
   );
 }
